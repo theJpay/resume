5e31a734e70d252761047aab2034892a2aa391f5
Jules Paris <jules.paris@wanadoo.fr>
2019-01-23 17:10:35 +0100

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,26 @@ import { HeaderComponent } from "./header/header.component";
 import { BsDropdownModule } from "ngx-bootstrap/dropdown";
 import { TooltipModule } from "ngx-bootstrap/tooltip";
 import { ModalModule } from "ngx-bootstrap/modal";
-import { AboutmeComponent } from './aboutme/aboutme.component';
-import { FencingComponent } from './fencing/fencing.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { EducationComponent } from './education/education.component';
+import { PopoverModule } from "ngx-bootstrap/popover";
+
+import { AboutmeComponent } from "./aboutme/aboutme.component";
+import { FencingComponent } from "./fencing/fencing.component";
+import { ExperienceComponent } from "./experience/experience.component";
+import { EducationComponent } from "./education/education.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, AboutmeComponent, FencingComponent, ExperienceComponent, EducationComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AboutmeComponent,
+    FencingComponent,
+    ExperienceComponent,
+    EducationComponent
+  ],
   imports: [
     BrowserModule,
     BsDropdownModule.forRoot(),
+    PopoverModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot()
   ],
